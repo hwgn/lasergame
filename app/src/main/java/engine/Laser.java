f55d3ec270ace0dffc8abfc53808c7cb1a0b3f98
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2021-05-12 15:24:35 +0200

    Mirror bugfix

--- app/src/main/java/engine/Laser.java
+++ app/src/main/java/engine/Laser.java
@@ -44,7 +44,7 @@ public class Laser {
                 break;
 
             if (tiles.get(pos).getType().equals(Tile.Type.MIRROR)) {
-                if (tiles.get(pos).getState() == rotation) rotation++;
+                if (tiles.get(pos).getState() == rotation) rotation = (rotation + 1) % 4;
                 else if (tiles.get(pos).getState() == (rotation + 1) % 4) rotation = (3 + rotation) % 4;
                 else break;
 
@@ -64,13 +64,15 @@ public class Laser {
     }
 
     private static Pair<Integer, Integer> getNextPosition(Pair<Integer, Integer> pos, int rotation) {
-        switch (rotation) {
-            case 0 -> pos = Pair.of(pos.x(), pos.y() - 1);
-            case 1 -> pos = Pair.of(pos.x() + 1, pos.y());
-            case 2 -> pos = Pair.of(pos.x(), pos.y() + 1);
-            case 3 -> pos = Pair.of(pos.x() - 1, pos.y());
-        }
-        return pos;
+
+        return switch (rotation) {
+            case 0 -> Pair.of(pos.x(), pos.y() - 1);
+            case 1 -> Pair.of(pos.x() + 1, pos.y());
+            case 2 -> Pair.of(pos.x(), pos.y() + 1);
+            case 3 -> Pair.of(pos.x() - 1, pos.y());
+            default -> throw new IllegalStateException("Unexpected value: " + rotation);
+        };
+
     }
 
     public Color getColor() {
