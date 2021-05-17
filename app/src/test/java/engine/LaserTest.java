a218d9bf72a763cc0e75cb05b2ac0d63c2f348fa
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2021-05-17 19:16:24 +0200

    Small improvement and idk where

---
+++ app/src/test/java/engine/LaserTest.java
@@ -0,0 +1,29 @@
+package engine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getLasers() {
+    }
+
+    @Test
+    void color() {
+    }
+
+    @Test
+    void points() {
+    }
+
+    @Test
+    void isComplete() {
+    }
+}
